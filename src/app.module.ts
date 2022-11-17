fce6611251f0b55d87be42594d41ef2665ae1818
Rovian Vieceli <rovianvieceli@gmail.com>
2022-11-17 11:46:18 -0300

    [TypeORM - Entity] Adicionado TypeORM mapeando a conexão com o banco mysql e adicionando a entidade course para TypeORM adicionar ao banco usando a configuração de sincronia definido na AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './module/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'sample',
+      autoLoadEntities: true,
+      synchronize: true,
+      type: 'mysql',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
