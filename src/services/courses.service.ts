2404ac70f8edfd063ba0197379f55721828d08ef
Rovian Vieceli <rovianvieceli@gmail.com>
2022-11-17 22:49:16 -0300

    [TypeORM - Mapping] Relacionamento Many to Many (Muitos para Muitos)

--- src/services/courses.service.ts
+++ src/services/courses.service.ts
@@ -4,12 +4,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCourseDto } from '../domain/create-course.dto';
 import { UpdateCourseDto } from '../domain/update-course.dto';
+import { Tag } from '../entities/tag.entity';
 
 @Injectable()
 export class CoursesService {
   public constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
 
   public findAllCourse() {
@@ -25,13 +28,22 @@ export class CoursesService {
     return course;
   }
 
-  public createCourse(crateCourse: CreateCourseDto) {
-    const course = this.courseRepository.create(crateCourse);
+  public async createCourse(createCourseDto: CreateCourseDto) {
+    const tags = await Promise.all(
+      createCourseDto.tags.map((name: string) => this.preloadTagByName(name)),
+    );
+    const data = { ...createCourseDto, tags };
+    const course = this.courseRepository.create(data);
     return this.courseRepository.save(course);
   }
 
-  public async updateCourse(id: number, updateCourse: UpdateCourseDto) {
-    const data = { id: id, ...updateCourse };
+  public async updateCourse(id: number, updateCourseDto: UpdateCourseDto) {
+    const tags =
+      updateCourseDto.tags &&
+      (await Promise.all(
+        updateCourseDto.tags.map((name: string) => this.preloadTagByName(name)),
+      ));
+    const data = { id: id, ...updateCourseDto, tags };
     const course = await this.courseRepository.preload(data);
     if (!course) {
       throw new NotFoundException(`Course ID #${id} Not Found`);
@@ -45,5 +57,14 @@ export class CoursesService {
       throw new NotFoundException(`Course ID #${id} Not Found`);
     }
     return this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOneBy({ name: name });
+    if (tag) {
+      return tag;
+    }
+
+    return this.tagRepository.create({ name });
   }
 }
